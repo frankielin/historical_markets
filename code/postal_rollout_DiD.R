9720b831e3b26243fde7eed2c9d1253a5f819887
frankielin <frankie.lin@berkeley.edu>
2025-09-30 02:59:11 -0500

    Added CS DiD Analysis

--- code/postal_rollout_DiD.R
+++ code/postal_rollout_DiD.R
@@ -12,18 +12,18 @@ library(ggplot2)
 
 ## Setting File Location 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-data_location <- "./Desktop/historical_markets"
 
 ##################
 ## Loading Data ##
 ##################
 ## Load postal panel data
-postal_panel_dat <- fread(paste0(data_location,"/data/cleaned/postal_rollout_panel.csv"))
+postal_panel_dat <- fread(paste0("../data/clean/postal_rollout_panel.csv"))
 print(colnames(postal_panel_dat))
 
 ## Loading the rolled postal data
 ## This data is the counties where there is already postal service
-npostal_panel_dat <- fread(paste0(data_location,"/data/cleaned/non_postal_rollout_panel.csv"))
+npostal_panel_dat <- fread(paste0("../data/clean/non_postal_rollout_panel.csv"))
+
 
 ###################
 ## Data Cleaning ##
@@ -38,41 +38,60 @@ npostal_panel_dat[, town_state := paste0(Town, State)]
 npostal_panel_dat[, town_state_id := as.integer(factor(town_state))]
 npostal_panel_dat[, Rollout_Year := 0]
 
+## Artificially Dropping 50% of towns with zero to mess with the standard errors
+## This emulates the decreasing the power
+## Step 1: find state-city combos with all_zero patients in 1835–1910
+eligible_towns <- npostal_panel_dat[
+  Year >= 1835 & Year <= 1896,
+  .(all_zero = all(has_patient == 0)),
+  by = .(State, Town)
+][all_zero == TRUE, .(State, Town)]
+
+## Step 2: randomly select ~75% of those combos
+set.seed(777)  # reproducible
+drop_towns <- eligible_towns[
+  sample(.N, size = floor(0.75 * .N))
+]
+
+## Step 3: drop *all rows* from those combos
+npostal_panel_dat <- npostal_panel_dat[!drop_towns, on = .(State, Town)]
+
 ## Stacked Data (For analyzing )
 all_panel_dat <- rbind(postal_panel_dat, npostal_panel_dat, fill = TRUE)
 all_panel_dat[, town_state_id := as.integer(factor(town_state))]
 
 
-###################################
-## Callaway-Sant'anna Unbalanced ##
-###################################
+#########################
+## Callaway-Sant'anna  ##
+#########################
 #### Raw Callaway Sant'anna
 ## Using the permantly treated as the control
 first_roll = 1896
 last_roll = 1903
-reg_dat <- all_panel_dat[(Year >= (first_roll-2)) & (Year <= (last_roll+5))]
+reg_dat <- all_panel_dat[(Year >= (first_roll-10)) & (Year <= (last_roll+10))]
 
 ## Show the distribution of number of cities that this is rolled out to per year
-reg_dat[Rollout_Date!=0, .(unique_ids = uniqueN(town_state_id)), by = Year]
+rolled_out_count = reg_dat[Rollout_Date<= Year, .(unique_ids = uniqueN(town_state_id)), by = Rollout_Year]
+rolled_out_count
+table(reg_dat[,Year])
 
+## Dropping the small sample data
+# bad_towns <- unique(reg_dat[Rollout_Year %in% c(1896, 1897, 1898, 1899), town_state_id])
+# reg_dat <- reg_dat[!town_state_id %in% bad_towns]
 
-# Summary of ATT by cohort and time
-table(is.na(reg_dat$has_patient))
-table(is.na(reg_dat$Year))
-table(is.na(reg_dat$town_state_id))
-table(is.na(reg_dat$Rollout_Year))
+## Running CSDiD
+# RHS_desc = "Number of Patents"
+# RHS_var  = "N_patients"
 
-table(reg_dat$has_patient)
-table(reg_dat$Year)
-table(reg_dat$town_state_id)
-table(reg_dat$Rollout_Year)
+RHS_desc = "Probability of Having a Patient"
+RHS_var  = "has_patient"
 
 gt_out <- att_gt(
-  yname = "has_patient", 
+  yname = RHS_var, 
   tname = "Year",             
   idname = "town_state_id",   
   gname = "Rollout_Year",
-  allow_unbalanced_panel = FALSE,
+  allow_unbalanced_panel = TRUE,
   control_group = "nevertreated",
   est_method = "reg",
   data = reg_dat
@@ -86,7 +105,7 @@ event_study <- aggte(gt_out, type = "dynamic")
 summary(event_study)
 
 ggdid(event_study) + 
-  labs(x = "Event Time (Years relative to treatment)", y = "ATT") +
+  labs(x = "Event Time (Years Relative to Postal Service Access)", y = "ATT") +
   theme_minimal()
 
 ## Group Analysis
@@ -102,295 +121,74 @@ calendar_att <- aggte(gt_out, type = "calendar")
 summary(calendar_att)
 
 ## Overall Effect
+## Overall effect calculates across the entire sample
 overall_att <- aggte(gt_out, type = "simple") 
 summary(overall_att)
 
+##########################
+## Creating Event Study ##
+##########################
+#### Use the estimates from t +/- 9 to to create event study  
+## Keep only event times from -9 to 9
+keep_idx <- which(event_study$egt >= -9 & event_study$egt <= 9)
 
-#################################
-## Callaway-Sant'anna Balanced ##
-#################################
-## Here I maintain a balanced regression by keeping all time periods that are 
-## within 10 +/- years of a transition, I hold the entire sample of
-## untreated sample consistent throughout the entire time period
-bal_reg_dat <- all_panel_dat
-bal_reg_dat[Rollout_Year != 0 ,year_since_rollout:= Year - Rollout_Year]
-bal_reg_dat <- bal_reg_dat[(Rollout_Year == 0) | (year_since_rollout >= -10) & (year_since_rollout <= -10)] 
+## Create a truncated event-study object
+event_study_trunc <- event_study
+event_study_trunc$egt       <- event_study$egt[keep_idx]
+event_study_trunc$att.egt   <- event_study$att.egt[keep_idx]
+event_study_trunc$se.egt    <- event_study$se.egt[keep_idx]
+event_study_trunc$att.agg   <- event_study$att.agg    
+event_study_trunc$se.agg    <- event_study$se.agg
 
-## Running Regression 
-bal_gt_out <- att_gt(
-  yname = "has_patient", 
-  tname = "Year",             
-  idname = "town_state_id",   
-  gname = "Rollout_Year",
-  allow_unbalanced_panel = FALSE,
-  control_group = "nevertreated",
-  est_method = "reg",
-  data = bal_reg_dat
+## Event Study
+event_study_plot <- 
+  ggdid(event_study_trunc) +
+  ggtitle(NULL) +
+  labs(
+    x = "Event Time (Years Relative to Postal Service Access)",
+    y = RHS_desc
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+event_study_plot
+
+ggsave(paste0("../figures/CSDiD_event_study_",RHS_var,".pdf"), 
+       plot = event_study_plot, 
+       width = 6, 
+       height = 4)
+
+
+#### Compute cohort-weighted net ATT
+## Create variable storing the 2x2 estimates and SEs
+gt_dt <- data.table(
+  g           = gt_out$group,      # cohort
+  time        = gt_out$t,        # relative to treatment
+  att         = gt_out$att,        # ATT estimate
+  se          = gt_out$se          # standard error
 )
+gt_dt[, time_to_treat := time - g ] 
 
-summary(gt_out)
+## Merging the cohort count per each estimated effect 
+gt_dt <- merge( 
+  gt_dt,
+  rolled_out_count,
+  by.x = "g",       
+  by.y = "Rollout_Year",  
+  all.x = TRUE
+)
+setnames(gt_dt, "unique_ids", "n_cohort")
 
-## Aggregate dynamic (event-study) effects
-bal_event_study <- aggte(bal_gt_out, type = "dynamic")
-summary(bal_event_study)
+## Compute weighted average ATT across all periods and cohorts
+manual_post_ATT <- sum(gt_dt[time_to_treat >= 0 & time_to_treat <= 9]$att * gt_dt[time_to_treat >= 0  & time_to_treat <= 9]$n_cohort) /
+  sum(gt_dt[time_to_treat >= 0  & time_to_treat <= 9]$n_cohort)
 
-ggdid(bal_event_study) + 
-  labs(x = "Event Time (Years relative to treatment)", y = "ATT") +
-  theme_minimal()
+manual_post_ATT 
 
+## Checking if the average effect across the entire time is the same as
+## the automatic calculation function
+check_post_ATT <- sum(gt_dt[time_to_treat >= 0]$att * gt_dt[time_to_treat >= 0]$n_cohort) /
+  sum(gt_dt[time_to_treat >= 0]$n_cohort)
 
-
-## Group Analysis
-## Earlier groups show less power (effect)
-## This is expected as these are typically smaller groups
-bal_group_att <- aggte(bal_gt_out, type = "group")
-summary(group_att)
-
-## Calendar Year Analysis
-## Earlier groups show less power (effect)
-## This is expected as these are typically smaller groups
-bal_calendar_att <- aggte(bal_gt_out, type = "calendar")
-summary(calendar_att)
-
-## Overall Effect
-bal_overall_att <- aggte(bal_gt_out, type = "simple") 
-summary(bal_overall_att)
-
-
- 
-# #########################
-# ## Regression Analysis ##
-# #########################
-# # 1) Create the subset for the event window
-# reg_min_frame <- -10
-# reg_max_frame <- 10
-# 
-# reg_dat <- postal_panel_dat[
-#   years_since_shift >= reg_min_frame & years_since_shift <= reg_max_frame
-# ]
-# 
-# # 2) Manually create dummies for each lead/lag
-# reg_dat[, change_tm10 := as.integer(years_since_shift == -10)]
-# reg_dat[, change_tm9  := as.integer(years_since_shift == -9)]
-# reg_dat[, change_tm8  := as.integer(years_since_shift == -8)]
-# reg_dat[, change_tm7  := as.integer(years_since_shift == -7)]
-# reg_dat[, change_tm6  := as.integer(years_since_shift == -6)]
-# reg_dat[, change_tm5  := as.integer(years_since_shift == -5)]
-# reg_dat[, change_tm4  := as.integer(years_since_shift == -4)]
-# reg_dat[, change_tm3  := as.integer(years_since_shift == -3)]
-# reg_dat[, change_tm2  := as.integer(years_since_shift == -2)]
-# reg_dat[, change_tm1  := as.integer(years_since_shift == -1)]
-# 
-# reg_dat[, change_tp0  := as.integer(years_since_shift == 0)]
-# reg_dat[, change_tp1  := as.integer(years_since_shift == 1)]
-# reg_dat[, change_tp2  := as.integer(years_since_shift == 2)]
-# reg_dat[, change_tp3  := as.integer(years_since_shift == 3)]
-# reg_dat[, change_tp4  := as.integer(years_since_shift == 4)]
-# reg_dat[, change_tp5  := as.integer(years_since_shift == 5)]
-# reg_dat[, change_tp6  := as.integer(years_since_shift == 6)]
-# reg_dat[, change_tp7  := as.integer(years_since_shift == 7)]
-# reg_dat[, change_tp8  := as.integer(years_since_shift == 8)]
-# reg_dat[, change_tp9  := as.integer(years_since_shift == 9)]
-# reg_dat[, change_tp10 := as.integer(years_since_shift == 10)]
-# 
-# 
-# 
-# 
-# event_dummies <- c(
-#   "change_tm10","change_tm9","change_tm8","change_tm7","change_tm6","change_tm5",
-#   "change_tm4","change_tm3",
-#   "change_tp0","change_tp1","change_tp2","change_tp3","change_tp4","change_tp5",
-#   "change_tp6","change_tp7","change_tp8","change_tp9","change_tp10"
-# )
-# 
-# 
-# out <- att_gt(
-#   yname = "lemp",
-#   gname = "first.treat",
-#   idname = "countyreal",
-#   tname = "year",
-#   xformla = ~1,
-#   data = mpdta,
-#   est_method = "reg"
-# )
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# # # Build formula
-# # rhs <- paste(event_dummies, collapse = " + ")
-# # fmla <- as.formula(paste("has_patient~", rhs, "| town_state + Year"))
-# # 
-# # # Run the regression
-# # diff_in_diff_model <- felm(fmla, data = reg_dat)
-# # 
-# # # Summary
-# # summary(diff_in_diff_model)
-# 
-# 
-# 
-# # # Example: drop all town_state that have any observation in 1886-1889
-# # years_to_exclude <- 1886:1890
-# # 
-# # # Find town_state to remove
-# # cities_to_drop <- unique(reg_dat[Year %in% years_to_exclude, town_state])
-# # 
-# # # Filter them out
-# # reg_dat <- reg_dat[!town_state %in% cities_to_drop]
-# 
-# # Check
-# reg_dat[, .N, by = Year]  # number of observations per year after filtering
-# 
-# ## The issue must be that there exists a country where there is only one year for like the first guy or last guy problems 
-# # Count unique cities per year
-# unique_cities_per_year <- reg_dat[, .(num_cities = uniqueN(town_state)), by = Year]
-# 
-# # View result
-# print(unique_cities_per_year)
-# 
-# 
-# # Define the full formula with all event dummies (except the baseline, e.g., change_tm1)
-# fmla <- as.formula(
-#   "N_patients ~ change_tm10 + change_tm9 + change_tm8 + change_tm7 + change_tm6 + change_tm5 +
-#    change_tm4 + change_tm3 +
-#    change_tp0 + change_tp1 + change_tp2 + change_tp3 + change_tp4 + change_tp5 +
-#    change_tp6 + change_tp7 + change_tp8 + change_tp9 + change_tp10 | town_state + Year")
-# 
-# # Run the regression
-# model <- felm(fmla, data = reg_dat)
-# 
-# # Summary (felm will automatically drop any collinear dummies and tell you which ones)
-# summary(model)
-# ############################
-# ## Setting up Environment ##
-# ############################
-# rm(list = ls())
-# 
-# ## Loading Packages
-# library(lfe)
-# library(data.table)
-# library(stargazer)
-# 
-# ## Setting File Location 
-# data_location <- "./Desktop/historical_markets/"
-# 
-# ##################
-# ## Loading Data ##
-# ##################
-# # Load postal panel data
-# postal_panel_dat <- fread(paste0(data_location,"./data/cleaned/postal_rollout_panel.csv"))
-# print(colnames(postal_panel_dat))
-# 
-# 
-# ###################
-# ## Data Cleaning ##
-# ###################
-# ## Creating Variables of Analysis
-# postal_panel_dat[, town_state := paste0(Town, State)]
-# postal_panel_dat[, years_since_shift := Year - Rollout_Year]
-# 
-# 
-# #########################
-# ## Regression Analysis ##
-# #########################
-# # 1) Create the subset for the event window
-# reg_min_frame <- -10
-# reg_max_frame <- 10
-# 
-# reg_dat <- postal_panel_dat[
-#   years_since_shift >= reg_min_frame & years_since_shift <= reg_max_frame
-# ]
-# 
-# # 2) Manually create dummies for each lead/lag
-# reg_dat[, change_tm10 := as.integer(years_since_shift == -10)]
-# reg_dat[, change_tm9  := as.integer(years_since_shift == -9)]
-# reg_dat[, change_tm8  := as.integer(years_since_shift == -8)]
-# reg_dat[, change_tm7  := as.integer(years_since_shift == -7)]
-# reg_dat[, change_tm6  := as.integer(years_since_shift == -6)]
-# reg_dat[, change_tm5  := as.integer(years_since_shift == -5)]
-# reg_dat[, change_tm4  := as.integer(years_since_shift == -4)]
-# reg_dat[, change_tm3  := as.integer(years_since_shift == -3)]
-# reg_dat[, change_tm2  := as.integer(years_since_shift == -2)]
-# reg_dat[, change_tm1  := as.integer(years_since_shift == -1)]
-# 
-# reg_dat[, change_tp0  := as.integer(years_since_shift == 0)]
-# reg_dat[, change_tp1  := as.integer(years_since_shift == 1)]
-# reg_dat[, change_tp2  := as.integer(years_since_shift == 2)]
-# reg_dat[, change_tp3  := as.integer(years_since_shift == 3)]
-# reg_dat[, change_tp4  := as.integer(years_since_shift == 4)]
-# reg_dat[, change_tp5  := as.integer(years_since_shift == 5)]
-# reg_dat[, change_tp6  := as.integer(years_since_shift == 6)]
-# reg_dat[, change_tp7  := as.integer(years_since_shift == 7)]
-# reg_dat[, change_tp8  := as.integer(years_since_shift == 8)]
-# reg_dat[, change_tp9  := as.integer(years_since_shift == 9)]
-# reg_dat[, change_tp10 := as.integer(years_since_shift == 10)]
-# 
-# 
-# 
-# 
-# event_dummies <- c(
-#   "change_tm10","change_tm9","change_tm8","change_tm7","change_tm6","change_tm5",
-#   "change_tm4","change_tm3",
-#   "change_tp0","change_tp1","change_tp2","change_tp3","change_tp4","change_tp5",
-#   "change_tp6","change_tp7","change_tp8","change_tp9","change_tp10"
-# )
-# 
-# 
-# 
-# # # Build formula
-# # rhs <- paste(event_dummies, collapse = " + ")
-# # fmla <- as.formula(paste("has_patient~", rhs, "| town_state + Year"))
-# # 
-# # # Run the regression
-# # diff_in_diff_model <- felm(fmla, data = reg_dat)
-# # 
-# # # Summary
-# # summary(diff_in_diff_model)
-# 
-# 
-# 
-# # # Example: drop all town_state that have any observation in 1886-1889
-# # years_to_exclude <- 1886:1890
-# # 
-# # # Find town_state to remove
-# # cities_to_drop <- unique(reg_dat[Year %in% years_to_exclude, town_state])
-# # 
-# # # Filter them out
-# # reg_dat <- reg_dat[!town_state %in% cities_to_drop]
-# 
-# # Check
-# reg_dat[, .N, by = Year]  # number of observations per year after filtering
-# 
-# ## The issue must be that there exists a country where there is only one year for like the first guy or last guy problems 
-# # Count unique cities per year
-# unique_cities_per_year <- reg_dat[, .(num_cities = uniqueN(town_state)), by = Year]
-# 
-# # View result
-# print(unique_cities_per_year)
-# 
-# 
-# # Define the full formula with all event dummies (except the baseline, e.g., change_tm1)
-# fmla <- as.formula(
-#   "N_patients ~ change_tm10 + change_tm9 + change_tm8 + change_tm7 + change_tm6 + change_tm5 +
-#    change_tm4 + change_tm3 +
-#    change_tp0 + change_tp1 + change_tp2 + change_tp3 + change_tp4 + change_tp5 +
-#    change_tp6 + change_tp7 + change_tp8 + change_tp9 + change_tp10 | town_state + Year")
-# 
-# # Run the regression
-# model <- felm(fmla, data = reg_dat)
-# 
-# # Summary (felm will automatically drop any collinear dummies and tell you which ones)
-# summary(model)
-# 
-# 
-# ## The issue is that at a certain point, everyone is treated and therefore there is a multi colinearity issue where there is 
-# ## No variaiton to capiture the effect that we are looking for? I think? Let me  
-# 
-# 
-# 
-# 
-# 
-# 
+print(check_post_ATT)
+overall_att$overall.att
